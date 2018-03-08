78bf890b3eb831ea2da06bb6e74684a1275b3bfb
RSADenchik <WorldRSA@gmail.com>
2018-03-09 00:16:06 +0200

    navbar complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-    $('.nav-icon1').click(function(){
+    var nav_icon = $('.nav-icon1');
+    nav_icon.click(function(){
         $(this).toggleClass('open');
+
+        if($(this).hasClass('open')){
+            $('.main-screen').addClass('grad');
+        }
+            else {
+            $('.main-screen').removeClass('grad');
+        }
+
     });
+    return true;
 });
