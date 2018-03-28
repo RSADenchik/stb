b8aa07ac5db59a8ee850498bccd118a099a518b5
RSADenchik <WorldRSA@gmail.com>
2018-03-28 18:55:57 +0300

    full vertical mobile landing

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         // Overlay background if navbar menu open
 
         if($(this).hasClass('open')){
-            $('<div class="d-md-none overlay">&nbsp;</div>').prependTo('body');
+            $('<div class="visible-xs overlay">&nbsp;</div>').prependTo('body');
         }
             else {
             $('.overlay').remove();
@@ -16,5 +16,75 @@ $(document).ready(function(){
     });
 
     //end mobile navbar menu
+
+    // function scroll height +100vh
+
+    (function() {
+        var delay = false;
+
+        $(document).on('mousewheel DOMMouseScroll', function(event) {
+            event.preventDefault();
+            if(delay) return;
+
+            delay = true;
+            setTimeout(function(){delay = false},200)
+
+            var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+            var a= document.getElementsByTagName('section');
+            if(wd < 0) {
+                for(var i = 0 ; i < a.length ; i++) {
+                    var t = a[i].getClientRects()[0].top;
+                    if(t >= 40) break;
+                }
+            }
+            else {
+                for(var i = a.length-1 ; i >= 0 ; i--) {
+                    var t = a[i].getClientRects()[0].top;
+                    if(t < -20) break;
+                }
+            }
+            if(i >= 0 && i < a.length) {
+                $('html,body').animate({
+                    scrollTop: a[i].offsetTop
+                });
+            }
+        });
+    })();
+
+    // end function scroll height +100vh
+
+    // function scroll on click arrow-down
+
+    var home_arrow = $('#home-arrow');
+    var video_arrow = $('#video-arrow');
+    var photo_arrow = $('#photo-arrow');
+
+    home_arrow.click(function() {
+
+        $('html,body').animate({
+                scrollTop: $("#videoCarousel").offset().top},
+            'slow');
+        return false;
+    });
+
+    video_arrow.click(function() {
+
+        $('html,body').animate({
+                scrollTop: $("#photoCarousel").offset().top},
+            'slow');
+        return false;
+    });
+
+    photo_arrow.click(function() {
+
+        $('html,body').animate({
+                scrollTop: $(".contact-screen").offset().top},
+            'slow');
+        return false;
+    });
+
+    // end function scroll on click arrow-down
+
 
 });
